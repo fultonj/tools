3438cb75e83c3550c6ee019b30a7e48cd15fa68f
John Fulton <fulton@redhat.com>
2026-03-09 18:33:27 -0400

    switch 1-mon-at-home.sh and laptop-screen.sh to wayland

--- bin/1-mon-at-home.sh
+++ bin/1-mon-at-home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --primary --mode 2560x1600 --pos 0x0 --rotate normal --output DP-4 --off
+wlr-randr --output eDP-1 --off --output DP-3 --on --mode 2560x1600 --pos 0,0 --transform normal
--- bin/laptop-screen.sh
+++ bin/laptop-screen.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off
+wlr-randr --output DP-3 --off --output eDP-1 --on --mode 1920x1200 --pos 0,0 --transform normal
