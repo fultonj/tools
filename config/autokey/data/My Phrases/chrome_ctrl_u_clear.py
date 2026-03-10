2468c7eedf10421d1570183cca405696432ec102
John Fulton <fulton@redhat.com>
2026-03-09 22:34:22 -0400

    remove i3/autokey/Xresources

--- config/autokey/data/My Phrases/chrome_ctrl_u_clear.py
+++
@@ -1,4 +0,0 @@
-import time
-keyboard.send_keys("<ctrl>+a")
-time.sleep(0.02)
-keyboard.send_keys("<delete>")
--- config/autokey/data/My Phrases/swallow_ctrl_k_in_chrome_beta.py
+++
@@ -1,3 +0,0 @@
-keyboard.send_keys("<shift>+<end>")
-keyboard.send_keys("<ctrl>+c")   # copy into clipboard
-keyboard.send_keys("<delete>")
